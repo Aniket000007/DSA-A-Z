e08000444b70ab2a92d7a674304491ea4ff103fd
Aniket Vijayaraj <aniket1042001@gmail.com>
2025-05-15 09:17:43 +0530

    goldman studies

--- kth_largest.java
+++ kth_largest.java
@@ -1,12 +1,42 @@
-package codeforces;
-
 import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
-public class practice {
+public class kth_largest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String []arr = {"tea", "ate", "anagram", "eat", "gramaan"};
+        ArrayList<String> result = giveAnagramDad(arr);
+        for(int i=0;i<result.size();i++){
+            System.out.println(result.get(i));
+        }
 	}
+
+    public static ArrayList<String> giveAnagramDad(String[] arr){
+        ArrayList<String> ans = new ArrayList<>();
+        HashMap<String,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            String str = SortString(arr[i]);
+            if(map.containsKey(str)){
+            }else{
+                ans.add(arr[i]);
+                map.put(str,0);
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+
+    public static String SortString(String str){
+        char[] chars = str.toCharArray();
+
+        Arrays.sort(chars);
+
+        String sortedStr = new String(chars);
+        return sortedStr;
+    }
 	
 	public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> qu = new PriorityQueue<>();
